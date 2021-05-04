7d1ca094fe3d05477b240bee75d02778e9c22338
yjloveslife <1174814249@qq.com>
2021-05-04 15:53:41 +0800

    first-commit

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,13 +3,16 @@ import Cookies from 'js-cookie'
 const TokenKey = 'vue_admin_template_token'
 
 export function getToken() {
-  return Cookies.get(TokenKey)
+  return localStorage.getItem('user_token')
+  // return Cookies.get(TokenKey)
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  return localStorage.setItem('user_token',token)
+  // return Cookies.set(TokenKey, token)
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+  return localStorage.removeItem('user_token')
+  // return Cookies.remove(TokenKey)
 }
